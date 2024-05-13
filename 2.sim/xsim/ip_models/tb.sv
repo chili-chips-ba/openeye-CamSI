5f64e2d6b9d1224f000381c59d14ccef5882fa5f
Robert M <RMetchev@hotmail.com>
2024-05-13 21:44:26 +0200

    ISERDES sim model

--- 2.sim/xsim/ip_models/tb.sv
+++ 2.sim/xsim/ip_models/tb.sv
@@ -6,15 +6,12 @@ logic clk_100;
 logic reset_ext;
 
 initial begin
-    clk_100 = 0;
-    forever #(5) clk_100 = ~clk_100;
+    clk_100 <= 0;
+    forever #(5) clk_100 <= ~clk_100;
 end        
 initial begin
     reset_ext = 1;
     #(150) reset_ext = 0;
-end        
-initial begin
-    #(100000) $finish;
 end        
 
 
@@ -41,5 +38,55 @@ fpga_pll_top fpga_pll_top (
     .clk_out1() // ~200MHz
 );
 
+// ISERDES
+logic clk_12p5;
+logic dphy_hs;
+
+
+initial begin
+    clk_12p5 <= 0;
+    forever begin
+        repeat(4) @(posedge clk_100);
+        #(0.0001) clk_12p5 <= ~clk_12p5;
+    end
+end        
+
+initial begin
+    dphy_hs = 0;
+    repeat (42) @(clk_100);
+    for (int i=0; i<100; i++) repeat (5) Send(i*3);
+    
+    @(clk_100);
+    dphy_hs <= 0;
+    repeat (200) @(clk_100);
+    $finish;
+end        
+
+
+
+csi_rx_phy_dat #(.INVERT(0), .DELAY(3)) csi_rx_phy_dat (
+   .bit_clock(clk_100),     // DDR bit clocks, buffered from D-PHY clock
+   .byte_clock(clk_12p5),   // byte clock = input clock /4
+
+   .reset(reset_ext),       // async reset, sync'd internally to byte clock
+   .enable(1),              // active-1 enable for SERDES
+
+   .dphy_hs({dphy_hs, ~dphy_hs}),                // lane input: [1]=P; [0]=N
+   .deser_out()             // deserialised byte output
+);
+
+logic P, N;
+logic [7:0] S, S1, S2, Z;
+always @(posedge clk_100) P <= csi_rx_phy_dat.in_delayed;
+always @(negedge clk_100) N <= csi_rx_phy_dat.in_delayed;
+always @(posedge clk_100) S <= {N, P, S[7:2]};
+always @(posedge clk_100) S1 <= S;
+always @(posedge clk_100) S2 <= S1;
+always @(posedge clk_12p5) Z <= S2;
+always @(posedge clk_12p5) $display(S, S1, S2, Z);
+
+task Send(input [7:0] s);
+    for (int i=0; i<8; i++) @(clk_100) dphy_hs <= s[i];
+endtask
 
 endmodule
