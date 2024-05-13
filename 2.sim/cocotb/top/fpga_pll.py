b232dbf245ab687df73ba2edbe54b72e4a8f01f0
Robert M <RMetchev@hotmail.com>
2024-05-13 16:00:52 +0200

    PLL models

---
+++ 2.sim/cocotb/top/fpga_pll.py
@@ -0,0 +1,50 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer, with_timeout
+
+class fpga_pll():
+    def __init__(self, dut, pll):
+        
+
+        if pll == 'top':
+            divclk_divide = 5           # Master division value (1-106)
+            clkfbout_mult_f = 49.875    # Multiply value for all CLKOUT (2.000-64.000)
+            clkout0_divide_f = 4.375
+            clkout2_divide = 5
+            self.start_up_time = 3595   # ns
+            self.lock_time = 9258.75    # ns
+            p = dut.u_clkrst_gen.u_pll_top
+            self.clk0 = p.uclk_out0
+            self.clk2 = p.uclk_out1
+            self.lock = p.pll_lock
+
+        if pll == 'hdmi':
+            divclk_divide = 3           # Master division value (1-106)
+            clkfbout_mult_f = 38.750    # Multiply value for all CLKOUT (2.000-64.000)
+            clkout0_divide_f = 3.0
+            clkout2_divide = 15
+            self.start_up_time = 4635   # ns
+            self.lock_time = 5707.5     # ns
+            p = dut.u_hdmi_top.u_hdmi_backend.u_fpga_pll_hdmi
+            self.clk0 = p.uclk_pix5
+            self.clk2 = p.uclk_pix
+            self.lock = p.pll_lock
+        
+        ref_clk = 100 # MHz
+        clk0_freq = ref_clk * clkfbout_mult_f / (divclk_divide * clkout0_divide_f)
+        clk2_freq = ref_clk * clkfbout_mult_f / (divclk_divide * clkout2_divide)
+        self.clk0_period = 2*round(1e3/clk0_freq/2, 3)      # ns # ValueError: Unable to accurately represent 4.385964912280702(ns) with the simulator precision of 1e-12
+        self.clk2_period = 2*round(1e3/clk2_freq/2, 3)      # ns
+
+        cocotb.start_soon(self.pll_clks())
+        cocotb.start_soon(self.pll_lock())
+
+    async def pll_lock(self):
+        self.lock.value = 0
+        await Timer(self.lock_time, units='ns')
+        self.lock.value = 1
+
+    async def pll_clks(self):
+        await Timer(self.start_up_time, units='ns')
+        cocotb.start_soon(Clock(self.clk0, self.clk0_period, units="ns").start())
+        cocotb.start_soon(Clock(self.clk2, self.clk2_period, units="ns").start())
---
+++ 2.sim/cocotb/top/test_top.py
@@ -0,0 +1,23 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge, Timer, with_timeout
+import os
+
+import fpga_pll
+
+
+@cocotb.test()
+async def test_0(dut):
+    # initializer PLL
+    pllt = fpga_pll.fpga_pll(dut, 'top')
+    pllh = fpga_pll.fpga_pll(dut, 'hdmi')
+
+    # Ext clock generation
+    cocotb.start_soon(Clock(dut.clk_ext, 10, units="ns").start())  # 100 MHz clock
+
+    # Initialize signals
+    dut.areset.value = 1
+    await Timer(3000, units='ns')
+    dut.areset.value = 0
+
+    await ClockCycles(dut.clk_ext, 1596)
