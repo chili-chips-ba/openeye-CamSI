f3bfa0c08c5132144a73babbd7ceae1ca4ee0c31
Juninho99 <azunic1@etf.unsa.ba>
2024-06-30 00:09:09 +0200

    Added and tested (on 2 lanes) CSI class

---
+++ 2.sim/cocotb/top/cocotbext_csi.py
@@ -0,0 +1,156 @@
+import cocotb
+from cocotb.triggers import Timer
+
+class CSI:
+   def __init__(self, dut, signal, period_ns, fps_Hz, line_length, frame_length, frame_blank, num_lane, dinvert):
+      self.dut = dut
+      self.signal = signal
+      self.period_ns = period_ns
+      self.fps_Hz = fps_Hz
+      self.line_length = line_length
+      self.frame_length = frame_length
+      self.frame_blank = frame_blank
+      self.num_lane = num_lane
+      self.dinvert = dinvert
+      self.temp_var = (1_000_000_000 / fps_Hz) / (8 * period_ns)
+      self.line_blank = int(self.temp_var / (frame_length + frame_blank)) - int(line_length / num_lane)
+
+   def invert_byte(self, byte, lane):
+      """
+      Function to invert byte based on the DINVERT setting.
+      """
+      if self.dinvert[lane]:
+         return ~byte & 0xFF
+      return byte
+
+   async def send_combined_bytes(self, byte0, byte1, byte2=0x00, byte3=0x00):
+      """
+      Coroutine to send two or four bytes combined on the differential signal.
+      The bytes are decomposed into bits, and each bit is mapped to a lane.
+      For differential signaling, each bit has a positive and a negative component.
+      """
+      for bit in range(8):
+         bit_a = (byte0 >> bit) & 0x1
+         bit_b = ~bit_a & 0x1
+         bit_c = (byte1 >> bit) & 0x1
+         bit_d = ~bit_c & 0x1
+
+         if self.num_lane == 2:
+            self.signal.value = (bit_b << 3) | (bit_a << 2) | (bit_d << 1) | bit_c
+         elif self.num_lane == 4:
+            bit_e = (byte2 >> bit) & 0x1
+            bit_f = ~bit_e & 0x1
+            bit_g = (byte3 >> bit) & 0x1
+            bit_h = ~bit_g & 0x1
+            self.signal.value = (bit_b << 7) | (bit_a << 6) | (bit_d << 5) | (bit_c << 4) | (bit_f << 3) | (bit_e << 2) | (bit_h << 1) | bit_g
+         
+         await Timer(self.period_ns, units='ns')
+
+   async def send_data_pattern(self, byte0, byte1, byte2=0x00, byte3=0x00, repeat_count=1):
+      """
+      Coroutine to send a specific data pattern on a given lane for a specified number of repetitions.
+      """
+      byte0 = self.invert_byte(byte0, 0)
+      byte1 = self.invert_byte(byte1, 1)
+      if self.num_lane == 4:
+         byte2 = self.invert_byte(byte2, 2)
+         byte3 = self.invert_byte(byte3, 3)
+
+      for _ in range(repeat_count):
+         if self.num_lane == 2:
+            await self.send_combined_bytes(byte0, byte1)
+         elif self.num_lane == 4:
+            await self.send_combined_bytes(byte0, byte1, byte2, byte3)
+
+   async def start_frame(self):
+      """
+      Coroutine to send the start of a frame.
+      """
+      if self.num_lane == 2:
+         await self.send_data_pattern(0x00, 0x00, repeat_count=1)  # Some random data at the start of the sequence
+         await self.send_data_pattern(0xB8, 0xB8, repeat_count=1)  # Sync bytes b8b8
+         await self.send_data_pattern(0x12, 0x00, repeat_count=1)  # Start of frame
+         await self.send_data_pattern(0x00, 0x00, repeat_count=6)  # Some random data at the end of the sequence
+      elif self.num_lane == 4: # adjust as needed
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 1)
+         await self.send_data_pattern(0xB8, 0xB8, 0xB8, 0xB8, 1)
+         await self.send_data_pattern(0x12, 0x00, 0x00, 0x00, 1)
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 6)
+
+   async def end_frame(self):
+      """
+      Coroutine to send the end of a frame.
+      """
+      if self.num_lane == 2:
+         await self.send_data_pattern(0x00, 0x00, repeat_count=1)  # Some random data at the start of the sequence
+         await self.send_data_pattern(0xB8, 0xB8, repeat_count=1)  # Sync bytes b8b8
+         await self.send_data_pattern(0x12, 0x01, repeat_count=1)  # End of frame
+         await self.send_data_pattern(0x00, 0x00, repeat_count=6)  # Some random data at the end of the sequence
+      elif self.num_lane == 4: # adjust as needed
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 1)
+         await self.send_data_pattern(0xB8, 0xB8, 0xB8, 0xB8, 1)
+         await self.send_data_pattern(0x12, 0x01, 0x00, 0x00, 1)
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 6)
+
+   async def send_embedded_data(self, data):
+      """
+      Coroutine to send embedded data.
+      """
+      length_high = (self.line_length >> 8) & 0xFF
+      length_low = self.line_length & 0xFF
+      
+      if self.num_lane == 2:
+         await self.send_data_pattern(0x00, 0x00, repeat_count=1)  # Some random data at the start of the sequence
+         await self.send_data_pattern(0xB8, 0xB8, repeat_count=1)  # Sync bytes b8b8
+         await self.send_data_pattern(length_low, 0x12, repeat_count=1)  # Start of long packet - EMBEDDED DATA
+         await self.send_data_pattern(0x11, length_high, repeat_count=1)
+         await self.send_data_pattern(data, data, repeat_count=int(self.line_length/self.num_lane)) # DATA
+         await self.send_data_pattern(0x00, 0x00, repeat_count=self.line_blank)  # Some random data at the end of the sequence
+      elif self.num_lane == 4: # adjust as needed
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 1)
+         await self.send_data_pattern(0xB8, 0xB8, 0xB8, 0xB8, 1)
+         await self.send_data_pattern(length_low, 0x12, 0x11, length_high, 1)
+         await self.send_data_pattern(data, data, data, data, int(self.line_length/self.num_lane))
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, self.line_blank)
+         
+   async def send_line(self, data):
+      """
+      Coroutine to send a line of data.
+      """
+      length_high = (self.line_length >> 8) & 0xFF
+      length_low = self.line_length & 0xFF
+      
+      if self.num_lane == 2:
+         await self.send_data_pattern(0x00, 0x00, repeat_count=1)  # Some random data at the start of the sequence
+         await self.send_data_pattern(0xB8, 0xB8, repeat_count=1)  # Sync bytes b8b8
+         await self.send_data_pattern(length_low, 0x28, repeat_count=1)  # Start of long packet - START OF LINE
+         await self.send_data_pattern(0x11, length_high, repeat_count=1)
+         await self.send_data_pattern(data, data, repeat_count=int(self.line_length/self.num_lane)) # DATA
+         await self.send_data_pattern(0x00, 0x00, repeat_count=self.line_blank)  # Some random data at the end of the sequence
+      elif self.num_lane == 4: # adjust as needed
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, 1)
+         await self.send_data_pattern(0xB8, 0xB8, 0x00, 0x00, 1)
+         await self.send_data_pattern(length_low, 0x28, 0x11, length_high, 1)
+         await self.send_data_pattern(data, data, data, data, int(self.line_length/self.num_lane))
+         await self.send_data_pattern(0x00, 0x00, 0x00, 0x00, self.line_blank)
+
+   async def send_frame(self):
+      """
+      Coroutine to send a full frame.
+      """
+      await self.start_frame()
+      line_data = 0x22  # Example data for a line
+
+      for i in range(5):  # (self.frame_length + self.frame_blank) instead of 5
+         if i > 2:        # (self.frame_length - 1) instead of 2
+            line_data = 0x11
+         await self.send_line(line_data)
+
+      await self.end_frame()
+
+   async def run(self, num_frames):
+      """
+      Coroutine to run the sequence of frame transmissions.
+      """
+      for _ in range(num_frames):
+         await self.send_frame()
--- 2.sim/cocotb/top/test_top.py
+++ 2.sim/cocotb/top/test_top.py
@@ -2,6 +2,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer, FallingEdge
 from cocotbext.i2c import I2cDevice
+from cocotbext_csi import CSI
 
 import os
 import fpga_pll
@@ -20,7 +21,7 @@ class i2c_slave_class:
       )
 
 @cocotb.test()
-async def test_0(dut):
+async def test_0(dut): #I2C Icarus
    # Get simulation runtime from environment variable, default to 15 microseconds
    run_sim_us = float(os.environ.get('RUN_SIM_US', 15))
    run_sim_cycles_100MHz = int(run_sim_us * 1000 / 10)
@@ -62,106 +63,7 @@ async def drive_diff_clock(signal, period_ns):
       signal.value = 0b01
       await Timer(period_ns / 2, units='ns')
 
-async def send_byte_on_lane(signal, lane, byte, period_ns):
-   """
-   Coroutine to send a specific byte on a given differential lane.
-   """
-   for bit in range(8):
-      bit_value = (byte >> bit) & 0x1
-      # Positive bit is at (lane*2 + 1), negative bit is at (lane*2)
-      signal[lane*2 + 1].value = bit_value  # Positive bit
-      signal[lane*2].value = ~bit_value & 0x1  # Negative bit (inverse)
-      await Timer(period_ns, units='ns')
-
-async def send_data_pattern(signal, lane, data_pattern, period_ns, duration_ns):
-   """
-   Coroutine to send a specific data pattern on a given lane for a specified duration.
-   """
-   start_time = cocotb.utils.get_sim_time(units='ns')
-   while cocotb.utils.get_sim_time(units='ns') - start_time < duration_ns:
-      await send_byte_on_lane(signal, lane, data_pattern, period_ns)
-
-async def send_sequence(signal, num_lanes, period_ns, sequence):
-   """
-   Coroutine to send a sequence of data patterns for specified durations on multiple lanes.
-   Each element in the sequence is a tuple (data_pattern, duration_ns).
-   """
-   for data_pattern, duration_ns in sequence:
-      for lane in range(num_lanes):
-         cocotb.start_soon(send_data_pattern(signal, lane, data_pattern[lane], period_ns, duration_ns))
-      await Timer(duration_ns, units='ns')
-
-@cocotb.test() #ICARUS
-async def test_1(dut):
-   # Current python model of PLL, will later be replaced with a standalone MMCM
-   pllt = fpga_pll.fpga_pll(dut, 'top')
-   pllh = fpga_pll.fpga_pll(dut, 'hdmi')
-
-   # Start external clock generation at 100 MHz
-   cocotb.start_soon(Clock(dut.clk_ext, 10, units="ns").start())
-
-   # Initialize reset signal
-   dut.areset.value = 1
-   await Timer(5000, units='ns')
-   dut.areset.value = 0
-
-   # Wait for Cam to start
-   await RisingEdge(dut.cam_en)
-   
-   # Start driving the differential clock signal
-   diff_clock_period_ns = 5
-   cocotb.start_soon(drive_diff_clock(dut.cam_dphy_clk, period_ns=diff_clock_period_ns))
-
-   await Timer(diff_clock_period_ns, units='ns')
-
-   # Define the sequence of data patterns and durations
-   sequence = [
-      ([0b00000000, 0b11111111], 40),  # Some random data at the start
-      ([0b10111000, 0b01000111], 20),  # Sync bytes b8b8
-      ([0b00010010, 0b11101101], 20),  # Start of long packet
-   ]
-
-   # Start driving the differential lane signals with the sequence of patterns
-   cocotb.start_soon(send_sequence(dut.cam_dphy_dat, num_lanes=2, period_ns=2.5, sequence=sequence))
-
-   # Wait for some time to allow further processing
-   await Timer(1, units='us')
-
-async def send_combined_bytes(signal, byte0, byte1, period_ns):
-   """
-   Coroutine to send two bytes combined on the differential signal.
-   The bits are mapped as follows:
-   - Lane 0: Positive bit is byte0, negative bit is inverted byte0
-   - Lane 1: Positive bit is byte1, negative bit is inverted byte1
-   """
-   for bit in range(8):
-      # Prepare the bit values for each lane
-      bit_a = (byte0 >> bit) & 0x1
-      bit_b = ~bit_a & 0x1
-      bit_c = (byte1 >> bit) & 0x1
-      bit_d = ~bit_c & 0x1
-
-      # Set the signal values for both lanes
-      signal.value = (bit_b << 3) | (bit_a << 2) | (bit_d << 1) | bit_c
-      await Timer(period_ns, units='ns')
-
-
-async def send_data_pattern1(signal, byte0, byte1, period_ns, repeat_count):
-    """
-    Coroutine to send a specific data pattern on a given lane for a specified number of repetitions.
-    """
-    for _ in range(repeat_count):
-        await send_combined_bytes(signal, byte0, byte1, period_ns)
-
-async def send_sequence1(signal, period_ns, sequence):
-   """
-   Coroutine to send a sequence of data patterns for specified durations on multiple lanes.
-   Each element in the sequence is a tuple (data_pattern, duration_ns).
-   """
-   for (byte0, byte1), repeat_count in sequence:
-      await send_data_pattern1(signal, byte0, byte1, period_ns, repeat_count)
-
-@cocotb.test()
+@cocotb.test() #CSI Verilator
 async def test_2(dut):
    # Current python model of PLL, will later be replaced with a standalone MMCM
    pllt = fpga_pll.fpga_pll(dut, 'top')
@@ -184,26 +86,10 @@ async def test_2(dut):
 
    await Timer(8*diff_clock_period_ns, units='ns')
 
-   # Define the sequence of data patterns and durations
-   sequence = [
-      ([0x00, 0xFF], 1),    # Some random data at the start
-      ([0xB8, 0x47], 1),    # Sync bytes b8b8
-      ([0x12, 0xFF], 1),    # Start of frame (FF inv. -> 00)
-      ([0x00, 0xFF], 7),    # Random data
-      ([0xB8, 0x47], 1),    # Sync bytes b8b8
-      ([0x32, 0xED], 1),    # Start of long packet (32h bytes, ED inv. -> 12) - EMBEDDED DATA
-      ([0x11, 0xFF], 1),    # (FF inv. -> 00), combined with previous 32 --> 0032 bytes to read
-      ([0xFF, 0x00], 25),   # Data for read
-      ([0x00, 0xFF], 7),    # Random data
-      ([0xB8, 0x47], 1),    # Sync bytes b8b8
-      ([0x32, 0xD7], 1),    # Start of long packet (32h bytes, D7 inv. -> 28) - START OF LINE
-      ([0x11, 0xFF], 1),    # (FF inv. -> 00), combined with previous 32 --> 0032 bytes to read
-      ([0x22, 0xDD], 32),   # Data for read
-   ]
-
-   # Start driving the differential lane signals with the sequence of patterns
-   cocotb.start_soon(send_sequence1(dut.cam_dphy_dat, period_ns=1.096, sequence=sequence))
-   await send_sequence1(dut.cam_dphy_dat, period_ns=1.096, sequence=sequence)
-
-   # Wait for some time to allow further processing
-   await Timer(1, units='us')
+   num_lane = 2
+   dinvert = [0, 1] if num_lane == 2 else [0, 1, 0, 1]
+    
+   csi = CSI(dut, dut.cam_dphy_dat, period_ns=1.096, fps_Hz=60, line_length=1280, frame_length=720, frame_blank=130, num_lane=num_lane, dinvert=dinvert)
+    
+   # Run the CSI class to send frames
+   await csi.run(3)  # Sending 3 frames as an example
