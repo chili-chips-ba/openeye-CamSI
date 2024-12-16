c320a7a10237862939985a1e962166d13a1ffb94
chili-chips-ba <ask@chili-chips.xyz>
2024-12-16 11:35:10 -0800

    Update top_pkg.sv

--- 1.hw/top_pkg.sv
+++ 1.hw/top_pkg.sv
@@ -42,27 +42,23 @@
 `ifndef __TOP_PKG__
 `define __TOP_PKG__
 
+//----------------Select one of our three supported sensors
 //`define IMX283
 `define IMX219
 //`define OV2740
 
+//----------------Include (or not) Ethernet?
 `define ETHERNET
 //`undef ETHERNET
+
+//----------------Trenz or Puzhitech board?
 `define PUZHI
 //`undef PUZHI
 
+//----------------Select monitor resolution
 `define HDMI_720p60
 //`define HDMI_1080p30
-//`define HDMI_1080p60 /*Artix-7 cannot do this option*/ 
-
-//`define MIPI_4_LANE
-`define MIPI_2_LANE
-//`define MIPI_1_LANE
-
-`define RAW8
-//`define RAW10
-//`define RAW12
-//`define RAW14
+//`define HDMI_1080p60 /*Artix-7 nominally cannot do this, but we push it*/ 
 
 
 package top_pkg;
@@ -92,12 +88,15 @@ package top_pkg;
 //-----------------------------------------------------------
 // Chip-specific
 //-----------------------------------------------------------
+
    localparam FPGA_DEV     = "7SERIES";
    localparam DPHY_TERM_EN = "TRUE";
+   
 
-
+//----------------------------------------
+// speed-up for sim
+//----------------------------------------
 `ifdef SIM_ONLY
- //speed-up for sim
    localparam NUM_CLK_FOR_400kHZ = 20;
    localparam NUM_CLK_FOR_1HZ    = 100; 
 `else
@@ -109,42 +108,80 @@ package top_pkg;
    typedef logic[$clog2(NUM_CLK_FOR_400kHZ) -1:0] cnt_400khz_t;
    typedef logic[$clog2(NUM_CLK_FOR_1HZ)    -1:0] cnt_1hz_t;
 
+   
+//----------------------------------------
+// derived controls
+//----------------------------------------
+`ifdef IMX283
+   `define MIPI_4_LANE
+   `define RAW12
 
+`elsif IMX219
+   `define MIPI_2_LANE
+   `define RAW10
+
+`elsif OV2740
+   `define MIPI_2_LANE
+   `define RAW8
+
+`else
+   `define MIPI_1_LANE
+   `define RAW8
+`endif
+
+
+//----------------------------------------
 // slave address of sensor
+//----------------------------------------
 `ifdef IMX283
    localparam bus7_t I2C_SLAVE_ADDR = 7'd26;
    localparam int    NUM_REGISTERS  = 58;
+
    `ifdef HDMI_720p60
       localparam string I2C_INIT_MEM_FILE = "i2c_init_IMX283_720p.mem";
    `else //HDMI_1080p60
       localparam string I2C_INIT_MEM_FILE = "i2c_init_IMX283_1080p.mem";
    `endif
+
+// - - - - - - - - - - - - - - - - - - - -
 `elsif IMX219
-   localparam bus7_t I2C_SLAVE_ADDR = 7'd16;
-   localparam int    NUM_REGISTERS  = 65;
+   localparam bus7_t I2C_SLAVE_ADDR    = 7'd16;
+   localparam int    NUM_REGISTERS     = 65;
    localparam string I2C_INIT_MEM_FILE = "i2c_init_IMX219.mem";
-`else //OV2740
-   localparam bus7_t I2C_SLAVE_ADDR = 7'd32;
-   localparam int    NUM_REGISTERS  = 155;
+
+// - - - - - - - - - - - - - - - - - - - -
+//OV2740
+`else 
+   localparam bus7_t I2C_SLAVE_ADDR    = 7'd32;
+   localparam int    NUM_REGISTERS     = 155;
    localparam string I2C_INIT_MEM_FILE = "i2c_init_OV2740.mem";
 `endif
    
+
+//----------------------------------------
 // number of CSI lanes
+//----------------------------------------
 `ifdef MIPI_4_LANE
    localparam                       NUM_LANE = 4; 
    localparam bit    [NUM_LANE-1:0] DINVERT  = 4'b0001; // based on Trenz board, adjust as needed
    localparam bus5_t [NUM_LANE-1:0] DSKEW    = {5'd1, 5'd1, 5'd1, 5'd1};
 
+// - - - - - - - - - - - - - - - - - - - -
 `elsif MIPI_2_LANE
-   localparam                       NUM_LANE = 2;  
-`ifdef PUZHI
-   localparam bit    [NUM_LANE-1:0] DINVERT  = 2'b00; // based on Puzhi board, adjust as needed
-`else
-   localparam bit    [NUM_LANE-1:0] DINVERT  = 2'b01; // based on Trenz board, adjust as needed (CRUVI A -> 2'b01, CRUVI C -> 2'b10)
-`endif
+   localparam                       NUM_LANE = 2;
+
+   
+   `ifdef PUZHI
+      localparam bit    [NUM_LANE-1:0] DINVERT  = 2'b00; // based on Puzhi board
+   `else
+      localparam bit    [NUM_LANE-1:0] DINVERT  = 2'b01; // based on Trenz board, adjust as needed (CRUVI A -> 2'b01, CRUVI C -> 2'b10)
+   `endif
+
    localparam bus5_t [NUM_LANE-1:0] DSKEW    = {5'd3, 5'd3};
 
-`else // MIPI_1_LANE is default
+// - - - - - - - - - - - - - - - - - - - -
+// MIPI_1_LANE is default
+`else 
    localparam                       NUM_LANE = 1; 
    localparam bit    [NUM_LANE-1:0] DINVERT  = 1'b0; // based on Trenz board, adjust as needed
    localparam bus5_t [NUM_LANE-1:0] DSKEW    = 5'd3;
@@ -156,15 +193,22 @@ package top_pkg;
    typedef logic  [NUM_LANE-1   :0] lane_vld_t;
    typedef bus8_t [NUM_LANE-1   :0] lane_data_t;
 
+   
+//----------------------------------------
+// Datalane width
+//----------------------------------------
 `ifdef RAW8
    typedef logic  [NUM_LANE*8-1 :0] lane_raw_data_t;
    typedef logic  [NUM_LANE*8-1 :0] lane_mem_t;
+
 `elsif RAW10
    typedef logic  [NUM_LANE*10-1:0] lane_raw_data_t;
    typedef logic  [NUM_LANE*10-1:0] lane_mem_t;
+
 `elsif RAW12
    typedef logic  [NUM_LANE*12-1:0] lane_raw_data_t;
    typedef logic  [NUM_LANE*12-1:0] lane_mem_t;
+
 `else // RAW14
    typedef logic  [NUM_LANE*14-1:0] lane_raw_data_t;
    typedef logic  [NUM_LANE*14-1:0] lane_mem_t;
